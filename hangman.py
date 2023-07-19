64825fe18768878c9fc445ba640b84575ae59556
stikkie13 <139982266+stikkie13@users.noreply.github.com>
2023-07-19 15:52:21 +0200

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,9 +1,9 @@
 class hangman:
     def __init__(self,word):
         self.word = list(word)
-        
         self.Guess = []
         self.Guess.extend(['_' for i in self.word])
+        self.score = 0
     
     def check(self, letter):
         if letter in self.word:
@@ -16,9 +16,13 @@ class hangman:
             return self.ShowGuess()
         else:
             wrongGuess = 'there is no \'{}\' in the word'
+            self.score += 1
+            print(self.score)
             return wrongGuess.format(letter) + '\n' + self.ShowGuess()
         
-    
+    def points(self):
+        return self.score >= 7
+
     def ShowGuess(self):
         x = ''.join(self.Guess)
         return x
